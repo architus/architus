40a268657b260f892dabd42f784ec35c474f4025
Jonathan Buchanan <jobuchanan@datto.com>
2018-12-17 11:09:13 -0500

    actual error messages

--- src/commands/set_command.py
+++ src/commands/set_command.py
@@ -1,4 +1,5 @@
 from src.commands.abstract_command import abstract_command
+from src.server_settings import server_settings
 from src.models import User, Admin, Command
 from discord import ChannelType
 from src.smart_command import smart_command, VaguePatternError
@@ -15,12 +16,17 @@ class set_command(abstract_command):
         smart_commands = kwargs['smart_commands']
         admins = kwargs['admins']
         server = self.server
-        from_admin = int(self.author.id) in admins[int(server.id)]
-        botcommands = discord.utils.get(self.server.channels, name='bot-commands', type=ChannelType.text)
-        if botcommands != self.channel and not from_admin and botcommands:
-            await self.client.send_message(self.channel, botcommands.mention + '?')
-            return
+        settings = server_settings(self.session, self.server)
+        from_admin = self.author.id in settings.admins_ids
+        if settings.bot_commands_channels and self.channel.id not in settings.bot_commands_channels and not from_admin:
+            for channelid in settings.bot_commands_channels:
+                botcommands = discord.utils.get(self.server.channels, id=channelid, type=ChannelType.text)
+                if botcommands:
+                    await self.client.send_message(self.channel, botcommands.mention + '?')
+                    return
+
         parser = re.search('!set (.+)::(.+)', self.content, re.IGNORECASE)
+        msg = "try actually reading the syntax"
         if parser and len(parser.group(2)) <= 200 and len(parser.group(1)) > 1 and server.default_role.mention not in parser.group(2) or from_admin:
             try:
                 command = smart_command(parser.group(1), parser.group(2), 0, server, self.author.id)
@@ -28,35 +34,38 @@ class set_command(abstract_command):
                 await self.client.send_message(self.channel, 'let\'s try making that a little more specific please')
                 return
 
-            if not any(command == oldcommand for oldcommand in smart_commands[int(server.id)]) and not len(command.raw_trigger) == 0:
+            if not any(command == oldcommand for oldcommand in smart_commands[int(server.id)]) and not len(command.raw_trigger) == 0 and command.raw_response not in ['remove', 'author']:
                 smart_commands[int(server.id)].append(command)
                 new_command = Command(server.id + command.raw_trigger, command.raw_response, command.count, int(server.id), self.author.id)
                 self.session.add(new_command)
                 self.session.commit()
-                await self.client.send_message(self.channel, 'command set')
-                return
+                msg = 'command set'
             elif parser.group(2) == "remove" or parser.group(2) == " remove":
                 for oldcommand in smart_commands[int(server.id)]:
                     if oldcommand == command:
                         smart_commands[int(server.id)].remove(oldcommand)
                         self.update_command(oldcommand.raw_trigger, '', 0, server, self.author.id, delete=True)
-                        await self.client.send_message(self.channel, 'removed')
-                        return
+                        msg = 'removed'
             elif parser.group(2) == "list" or parser.group(2) == " list":
                 for oldcommand in smart_commands[int(server.id)]:
                     if oldcommand == command:
-                        await self.client.send_message(self.channel, str(oldcommand))
-                        return
+                        msg = str(oldcommand)
+                    else:
+                        msg = 'no command with that trigger'
             elif parser.group(2) == "author" or parser.group(2) == " author":
+                msg = 'no command with that trigger'
                 for oldcommand in smart_commands[int(server.id)]:
                     if oldcommand == command:
                         try:
                             usr = await self.client.get_user_info(str(oldcommand.author_id))
-                            await self.client.send_message(self.channel, usr.name + '#' + usr.discriminator)
+                            msg = usr.name + '#' + usr.discriminator
                         except:
-                            await self.client.send_message(self.channel, 'idk')
-                        return
-        await self.client.send_message(self.channel, 'no')
+                            msg = 'idk'
+        elif parser and len(parser.group(2)) >= 200:
+            msg = 'too long, sorry. ask the owner to set it'
+        elif parser and len(parser.group(1)) > 1:
+            msg = 'too short'
+        await self.client.send_message(self.channel, msg)
 
     def get_help(self):
         return "Sets a custom command\nYou may include the following options:\n[noun],[adj],[adv],[member],[owl],[:reaction:],[count],[comma,separated,choices]"
