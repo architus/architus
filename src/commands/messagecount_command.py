f786d3e2746fcfc5a5efbe2f9ae673c4c519c25e
Jonathan Buchanan <johnyburd@gatech.edu>
2019-05-13 13:57:42 -0400

    unique names

--- src/commands/messagecount_command.py
+++ src/commands/messagecount_command.py
@@ -1,4 +1,5 @@
 from src.commands.abstract_command import abstract_command
+import random, string, os
 import src.generate.wordcount as wordcount_gen
 from discord import ChannelType
 import discord
@@ -37,8 +38,9 @@ class messagecount_command(abstract_command):
             except Exception as e: print(e)
         #await self.client.send_message(ctxchannel, "%s has sent %d words across %d messages" % (victim.display_name, word_counts[victim], message_counts[victim]))
 
-        wordcount_gen.generate('hello', message_counts, word_counts, victim)
-        with open('res/wordhello.png', 'rb') as f:
+        key = ''.join([random.choice(string.ascii_letters) for n in range(10)])
+        wordcount_gen.generate(key, message_counts, word_counts, victim)
+        with open('res/word%s.png' % key, 'rb') as f:
             channel = discord.utils.get(self.client.get_all_channels(), id=IMAGE_CHANNEL_ID)
 
             msg = await self.client.send_file(channel, f)
@@ -50,6 +52,8 @@ class messagecount_command(abstract_command):
             em.set_footer(text="{0} has sent {1:,} words across {2:,} messages".format(victim.display_name, word_counts[victim], message_counts[victim]), icon_url=victim.avatar_url)
 
         await self.client.send_message(self.channel, embed=em)
+
+        os.remove("res/word%s.png" % key)
 
 
 
