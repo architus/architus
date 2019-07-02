1e8d161b4d300c9b4cd21d0c82291e17a33ae354
Jonathan Buchanan <johnyburd@gatech.edu>
2019-07-01 23:18:31 -0400

    regexes

--- src/api/api.py
+++ src/api/api.py
@@ -3,94 +3,10 @@ import traceback
 import asyncio
 import secrets
 import websockets
+import re
 from discord.ext.commands import Cog, Context, MinimalHelpCommand
 
-class MockMember(object):
-    def __init__(self, id=0):
-        self.id = id
-        self.mention = "<@%_CLIENT_ID_%>"
-        self.display_name = "bad guy"
-        self.bot = False
-
-class MockRole(object):
-    pass
-
-class MockChannel(object):
-    def __init__(self, bot, sends, reactions, resp_id):
-        self.bot = bot
-        self.sends = sends
-        self.reactions = reactions
-        self.resp_id = resp_id
-    async def send(self, *args):
-        for thing in args:
-            self.sends.append(thing)
-        return MockMessage(self.bot, self.resp_id, self.sends, self.reactions, 0)
-
-class MockGuild(object):
-    def __init__(self, id):
-        self.region = 'us-east'
-        self.id = int(id)
-        self.owner = MockMember()
-        self.me = MockMember()
-        self.default_role = MockRole()
-        self.default_role.mention = "@everyone"
-        self.emojis = []
-
-    def get_member(self, *args):
-        return None
-
-class MockReact(object):
-    def __init__(self, message, emoji, user):
-        self.message = message
-        self.emoji = emoji
-        self.count = 1
-        self._users = [user]
-    def users(self):
-        class user:
-            pass
-        u = user()
-        async def flatten():
-            return self._users
-        u.flatten = flatten
-        return u
-
-class MockMessage(object):
-    def __init__(self, bot, id, sends, reaction_sends, guild_id, content=None, resp_id=0):
-        self.bot = bot
-        self.id = id
-        self.sends = sends
-        self.reaction_sends = reaction_sends
-        self._state = MockChannel(bot, sends, reaction_sends, resp_id)
-        self.guild = MockGuild(guild_id)
-        self.author = MockMember()
-        self.channel = MockChannel(bot, sends, reaction_sends, resp_id)
-        self.content = content
-        self.reactions = []
-    async def add_reaction(self, emoji, bot=True):
-        user = MockMember()
-        if bot:
-            self.reaction_sends.append((self.id, emoji))
-            user = self.bot
-        for react in self.reactions:
-            if emoji == react.emoji:
-                react._users.append(user)
-                return react
-        else:
-            react = MockReact(self, emoji, user)
-            self.reactions.append(react)
-            return react
-
-    async def remove_reaction(self, emoji):
-        user = MockMember()
-        remove_react = None
-        for react in self.reactions:
-            if emoji == react.emoji:
-                react._users = [self.bot.user]
-                return react
-
-    async def edit(self, content=None):
-        print("EDIT " + content)
-        self.sends.append(content)
+CALLBACK_URL = "https://aut-bot.com/app"
 
 class Api(Cog):
 
@@ -130,11 +46,23 @@ class Api(Cog):
 
     async def store_callback(self, nonce=None, url=None):
         assert nonce and url
+        if not any(re.match(pattern, url) for pattern in (
+                'https:\/\/[A-Fa-f0-9]{24}--aut-bot\.netlify\.com\/app',
+                'https:\/\/deploy-preview-[0-9]+--aut-bot\.netlify\.com\/app',
+                'https:\/\/aut-bot.com\/app',
+                'http:\/\/localhost:3000\/app')):
+            url = CALLBACK_URL
+            
         self.callback_urls[nonce] = url
         return {"content": True}
 
     async def get_callback(self, nonce=None):
-        resp = {"content": self.callback_urls[nonce]}
+        try:
+            url = self.callback_urls[nonce]
+        except KeyError:
+            print("couldn't find callback url, returning default")
+            url = CALLBACK_URL
+        resp = {"content": url}
         self.callback_urls[nonce] = None
         return resp
 
@@ -253,6 +181,92 @@ class Api(Cog):
             print(resp)
         return resp
 
+class MockMember(object):
+    def __init__(self, id=0):
+        self.id = id
+        self.mention = "<@%_CLIENT_ID_%>"
+        self.display_name = "bad guy"
+        self.bot = False
+
+class MockRole(object):
+    pass
+
+class MockChannel(object):
+    def __init__(self, bot, sends, reactions, resp_id):
+        self.bot = bot
+        self.sends = sends
+        self.reactions = reactions
+        self.resp_id = resp_id
+    async def send(self, *args):
+        for thing in args:
+            self.sends.append(thing)
+        return MockMessage(self.bot, self.resp_id, self.sends, self.reactions, 0)
+
+class MockGuild(object):
+    def __init__(self, id):
+        self.region = 'us-east'
+        self.id = int(id)
+        self.owner = MockMember()
+        self.me = MockMember()
+        self.default_role = MockRole()
+        self.default_role.mention = "@everyone"
+        self.emojis = []
+
+    def get_member(self, *args):
+        return None
+
+class MockReact(object):
+    def __init__(self, message, emoji, user):
+        self.message = message
+        self.emoji = emoji
+        self.count = 1
+        self._users = [user]
+    def users(self):
+        class user:
+            pass
+        u = user()
+        async def flatten():
+            return self._users
+        u.flatten = flatten
+        return u
+
+class MockMessage(object):
+    def __init__(self, bot, id, sends, reaction_sends, guild_id, content=None, resp_id=0):
+        self.bot = bot
+        self.id = id
+        self.sends = sends
+        self.reaction_sends = reaction_sends
+        self._state = MockChannel(bot, sends, reaction_sends, resp_id)
+        self.guild = MockGuild(guild_id)
+        self.author = MockMember()
+        self.channel = MockChannel(bot, sends, reaction_sends, resp_id)
+        self.content = content
+        self.reactions = []
+    async def add_reaction(self, emoji, bot=True):
+        user = MockMember()
+        if bot:
+            self.reaction_sends.append((self.id, emoji))
+            user = self.bot
+        for react in self.reactions:
+            if emoji == react.emoji:
+                react._users.append(user)
+                return react
+        else:
+            react = MockReact(self, emoji, user)
+            self.reactions.append(react)
+            return react
+
+    async def remove_reaction(self, emoji):
+        user = MockMember()
+        remove_react = None
+        for react in self.reactions:
+            if emoji == react.emoji:
+                react._users = [self.bot.user]
+                return react
+
+    async def edit(self, content=None):
+        print("EDIT " + content)
+        self.sends.append(content)
 
 def setup(bot):
     bot.add_cog(Api(bot))
