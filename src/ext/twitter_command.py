1618d58fa094cea13df547f592ce669a013cf6b4
Nick <nbennett30@gatech.edu>
2019-07-09 21:34:00 -0500

    Update twitter_command.py

--- src/ext/twitter_command.py
+++ src/ext/twitter_command.py
@@ -1,10 +1,21 @@
 import twitter
 from discord.ext import commands
 
-api = twitter.Api(consumer_key='uhs3HzukyZvni2VU8rv5lmCk3',
-                  consumer_secret='uiWwYXf7s0tiA0Xpez7FA1EyWR9gp5c7w7Pqqf6ydJt2FHCjax',
-                  access_token_key='1148731389128450048-sMxNHBoXfFTYq4q3phVv8NtoyE2lE7',
-                  access_token_secret='zGHswZfMncpyYyvsSJ9pkalIl4K4DHgvhjkzyYy6zx6fk')
+try:
+    lines = [line.rstrip('\n') for line in open('.secret_token')]
+    consumer_key = lines[5]
+    consumer_secret = lines[6]
+    access_token_key = lines[7]
+    access_token_secret = lines[8]
+except Exception as e:
+    print(e)
+    print('error reading .secret_token, make it you aut')
+
+
+api = twitter.Api(consumer_key=consumer_key,
+                  consumer_secret=consumer_secret,
+                  access_token_key=access_token_key,
+                  access_token_secret=access_token_secret)
 
 
 @commands.command(aliases=['ajax', 'masters', 'beat'])
