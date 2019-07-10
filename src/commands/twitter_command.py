aa812e42fa1947a25f9cfa85ce134b744416fb7a
Nick <nbennett30@gatech.edu>
2019-07-09 20:16:49 -0500

    Update twitter_command.py

--- src/commands/twitter_command.py
+++ src/commands/twitter_command.py
@@ -1,10 +1,7 @@
 import twitter
 from discord.ext import commands
 
-api = twitter.Api(consumer_key='uhs3HzukyZvni2VU8rv5lmCk3',
-                  consumer_secret='uiWwYXf7s0tiA0Xpez7FA1EyWR9gp5c7w7Pqqf6ydJt2FHCjax',
-                  access_token_key='1148731389128450048-sMxNHBoXfFTYq4q3phVv8NtoyE2lE7' ,
-                  access_token_secret='zGHswZfMncpyYyvsSJ9pkalIl4K4DHgvhjkzyYy6zx6fk')
+
 
 @commands.command(aliases=['ajax', 'masters', 'beat'])
 async def ajax_masters(ctx):
