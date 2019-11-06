6d87488348d89d983f6c11947852849778225da0
Jonathan Buchanan <johnyburd@gatech.edu>
2019-11-05 23:14:26 -0500

    add hoar_frost generator and start new autoresponses class

---
+++ lib/hoar_frost.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timezone, timedelta
+from uuid import getnode
+from os import getpid
+
+class HoarFrostGenerator:
+
+    def __init__(self):
+        # discord's epoch
+        self.epoch = datetime(2015, 1, 1, tzinfo=timezone.utc)
+        self.increment = 0
+
+    def _calculate_timestamp(self):
+        return (datetime.now(timezone.utc) - self.epoch) // timedelta(milliseconds=1)
+
+    def generate(self):
+        hoar_frost = self._calculate_timestamp() << 22
+        hoar_frost |= (getnode() & 0b11111) << 17
+        hoar_frost |= (getpid() & 0b11111) << 12
+        hoar_frost |= self.increment & 0b111111111111
+        self.increment += 1
+        return hoar_frost
---
+++ shard/src/auto_response.py
@@ -0,0 +1,63 @@
+import string
+from contextlib import suppress
+
+
+class AutoResponse:
+
+    def __init__(
+        self,
+        bot,
+        trigger,
+        response,
+        author_id,
+        guild_id,
+        id=None,
+        trigger_regex=None,
+        trigger_punctuation=(),
+        response_ast=None,
+        mode=None,
+        count=0
+    ):
+        self.bot = bot
+        self.trigger = trigger
+        self.response = response
+        self.author_id = author_id
+        self.guild_id = guild_id
+        self.count = count
+
+        self.id = id or bot.hoar_frost_generator.generate()
+        self.response_ast = response_ast or self._parse_response()
+        self.mode = mode or self._determine_mode()
+
+        if self.mode == 'punctuated' and trigger_punctuation == ():
+            self.trigger_punctuation = self._extract_punctuation()
+        else:
+            self.trigger_punctuation = trigger_punctuation
+
+        self.trigger_regex = trigger_regex or self._generate_trigger_regex()
+
+    def _generate_trigger_regex(self):
+        # TODO
+        if self.mode == 'regex':
+            self.trigger_regex = self.trigger
+        elif self.mode == 'punctuated':
+            pass
+        else:
+            pass
+
+    def _extract_punctuation(self):
+        return tuple(c for c in self.trigger if c in string.punctuation)
+
+    def _parse_response(self):
+        # TODO
+        pass
+
+    def _determine_mode(self):
+        with suppress(IndexError):
+            if self.trigger[0] == '^' and self.trigger[-1] == '$':
+                return 'regex'
+
+        if any(c for c in self.trigger if c in string.punctuation):
+            return 'punctuated'
+
+        return 'naive'
