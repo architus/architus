17eb9e1538d796ae689a6f2fe2fde1a13604d9b0
Jonathan Buchanan <johnyburd8@gmail.com>
2019-12-13 14:30:56 -0500

    just some random stuff

--- shard/src/auto_response.py
+++ shard/src/auto_response.py
@@ -1,5 +1,6 @@
 import string
 from contextlib import suppress
+from typing import List
 
 
 class ResponseMode:
@@ -43,20 +44,28 @@ class AutoResponse:
 
         self.trigger_regex = trigger_regex or self._generate_trigger_regex()
 
+    def validate(self, settings: Setting, response_list: List[AutoResponse] = ()):
+        if len(self.trigger) < settings.resp_min_trigger_len:
+            raise ShortTriggerException(f"Trigger is under minimum length ({settings.resp_min_trigger_len})")
+        if len(self.response) > settings.resp_max_resp_len:
+            raise LongResponseException(f"Response is over maximum length ({settings.resp_max_resp_len})")
+
+        if any(intersects(self.trigger_regex, other.trigger_regex) for other in response_list):
+            raise TriggerCollisionException("Trigger collides with another already set")
+
     def _generate_trigger_regex(self):
         # TODO
-        if self.mode == ResponseMode.REGEX:
+        if self.mode in ResponseMode.REGEX:
             self.trigger_regex = self.trigger
         elif self.mode == ResponseMode.PUNCTUATED:
+            # escape regex stuff
+            # trim whitespace
             pass
         else:
+            # escape regex stuff
+            # filter punction
+            # trim whitespace
             pass
-
-        if self._collision_detector():
-            raise TriggerCollisionException()
-
-    def _collision_detector():
-        return False
 
     def _extract_punctuation(self):
         return tuple(c for c in self.trigger if c in string.punctuation)
@@ -77,4 +86,8 @@ class AutoResponse:
 
 
 class TriggerCollisionException(Exception):
+    pass
+
+
+class ShortTriggerException(Exception):
     pass
