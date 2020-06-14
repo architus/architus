5c083b51d336689d58bbdb698c467282f1e37495
Joseph Azevedo <joseph.az@gatech.edu>
2020-06-14 03:52:40 -0400

    feat(logs): harden gateway ingress

--- logs/ingress/src/gateway/mod.rs
+++ logs/ingress/src/gateway/mod.rs
@@ -1,7 +1,7 @@
 mod channel;
 
 use lazy_static::lazy_static;
-use log::{debug, trace, warn};
+use log::{debug, warn};
 use logs_lib::id::{HoarFrost, IdProvisioner};
 use logs_lib::{time, ActionOrigin, ActionType};
 use serde::ser;
@@ -11,8 +11,11 @@ use serenity;
 use serenity::async_trait;
 use serenity::client::bridge::gateway::GatewayIntents;
 use serenity::model::event::Event;
+use serenity::model::guild::Guild;
 use serenity::model::id::{AuditLogEntryId, GuildId, UserId};
+use serenity::model::permissions::Permissions;
 use serenity::prelude::*;
+use std::sync::Arc;
 
 lazy_static! {
     /// Includes all guild-related events to signal to Discord that we intend to
@@ -71,20 +74,30 @@ impl Source {
     }
 }
 
-pub struct GatewayContext<'a> {
+pub struct GatewayContext {
     discord: Context,
-    id_provisioner: &'a IdProvisioner,
+    id_provisioner: Arc<IdProvisioner>,
+}
+
+impl GatewayContext {
+    pub async fn has_perms(&self, guild: &Guild, target: Permissions) -> bool {
+        let user_id = self.discord.cache.current_user().await.id;
+        let bot_perms = guild.member_permissions(user_id).await;
+        let mut base_perms = target;
+        base_perms.remove(bot_perms);
+        base_perms.is_empty()
+    }
 }
 
 /// Bot event handler (used to dispatch events to the main service)
 pub struct Handler {
-    id_provisioner: IdProvisioner,
+    id_provisioner: Arc<IdProvisioner>,
 }
 
 impl Default for Handler {
     fn default() -> Self {
         Self {
-            id_provisioner: IdProvisioner::new(),
+            id_provisioner: Arc::new(IdProvisioner::new()),
         }
     }
 }
@@ -93,73 +106,75 @@ impl Handler {
     pub fn new() -> Self {
         Default::default()
     }
-
-    async fn normalize(&self, raw_event: Event, context: Context) -> Option<NormalizedEvent> {
-        // Create a grouped context object
-        let ctx = GatewayContext {
-            discord: context,
-            id_provisioner: &self.id_provisioner,
-        };
-
-        match raw_event {
-            // handle all log ingestion events
-            Event::ChannelCreate(_) | Event::ChannelDelete(_) | Event::ChannelUpdate(_) => channel::handle(raw_event, ctx).await,
-            Event::Unknown(event) => {
-                warn!("Received Event::Unknown: {:?}", event);
-                Some(NormalizedEvent {
-                    action_type: ActionType::Unknown,
-                    id: self.id_provisioner.provision(),
-                    source: Source::gateway(&event),
-                    origin: ActionOrigin::Hybrid,
-                    timestamp: time::millisecond_ts(),
-                    subject_id: None,
-                    guild_id: None,
-                    agent_id: None,
-                    audit_log_id: None,
-                })
-            },
-            _ => None
-            // Event::ChannelPinsUpdate(_) => Self::handle(raw_event, EventType::ChannelPinsUpdate).await
-            // Event::GuildBanAdd(_) => Self::handle(raw_event, EventType::GuildBanAdd).await,
-            // Event::GuildBanRemove(_) => Self::handle(raw_event, EventType::GuildBanRemove).await,
-            // Event::GuildEmojisUpdate(_) => Self::handle(raw_event, EventType::GuildEmojisUpdate).await
-            // Event::GuildIntegrationsUpdate(_) => Self::handle(raw_event, EventType::GuildIntegrationsUpdate).await
-            // Event::GuildMemberAdd(_) => Self::handle(raw_event, EventType::GuildMemberAdd).await,
-            // Event::GuildMemberRemove(_) => Self::handle(raw_event, EventType::GuildMemberRemove).await
-            // Event::GuildMemberUpdate(_) => Self::handle(raw_event, EventType::GuildMemberUpdate).await
-            // Event::GuildMembersChunk(_) => Self::handle(raw_event, EventType::GuildMembersChunk).await
-            // Event::GuildRoleCreate(_) => Self::handle(raw_event, EventType::GuildRoleCreate).await,
-            // Event::GuildRoleDelete(_) => Self::handle(raw_event, EventType::GuildRoleDelete).await,
-            // Event::GuildRoleUpdate(_) => Self::handle(raw_event, EventType::GuildRoleUpdate).await,
-            // Event::GuildUnavailable(_) => Self::handle(raw_event, EventType::GuildUnavailable).await
-            // Event::GuildUpdate(_) => Self::handle(raw_event, EventType::GuildUpdate).await,
-            // Event::MessageCreate(_) => Self::handle(raw_event, EventType::MessageCreate).await,
-            // Event::MessageDelete(_) => Self::handle(raw_event, EventType::MessageDelete).await,
-            // Event::MessageDeleteBulk(_) => Self::handle(raw_event, EventType::MessageDeleteBulk).await
-            // Event::MessageUpdate(_) => Self::handle(raw_event, EventType::MessageUpdate).await,
-            // Event::ReactionAdd(_) => Self::handle(raw_event, EventType::ReactionAdd).await,
-            // Event::ReactionRemove(_) => Self::handle(raw_event, EventType::ReactionRemove).await,
-            // Event::ReactionRemoveAll(_) => Self::handle(raw_event, EventType::ReactionRemoveAll).await
-            // Event::VoiceStateUpdate(_) => Self::handle(raw_event, EventType::VoiceStateUpdate).await
-            // Event::VoiceServerUpdate(_) => Self::handle(raw_event, EventType::VoiceServerUpdate).await
-            // Event::WebhookUpdate(_) => Self::handle(raw_event, EventType::WebhookUpdate).await,
-        }
-    }
 }
 
 #[async_trait]
 impl RawEventHandler for Handler {
     /// triggers on every event coming in from the gateway
-    async fn raw_event(&self, context: Context, raw_event: Event) {
-        trace!("Event: {:?}", raw_event);
-        match self.normalize(raw_event, context).await {
-            None => {}
-            Some(event) => {
-                if let Ok(json_str) = serde_json::to_string(&event) {
-                    debug!("{}", json_str);
+    async fn raw_event(&self, base_context: Context, event: Event) {
+        // Wraps the context in a single object
+        let context = GatewayContext {
+            discord: base_context,
+            id_provisioner: Arc::clone(&self.id_provisioner),
+        };
+
+        // Fork every handler to let it execute long-running tasks
+        tokio::spawn(async move {
+            match normalize(event, context).await {
+                None => {}
+                Some(event) => {
+                    if let Ok(json_str) = serde_json::to_string(&event) {
+                        debug!("{}", json_str);
+                    }
                 }
             }
-        }
+        });
+    }
+}
+
+async fn normalize(raw_event: Event, context: GatewayContext) -> Option<NormalizedEvent> {
+    match raw_event {
+        // handle all log ingestion events
+        Event::ChannelCreate(_) | Event::ChannelDelete(_) | Event::ChannelUpdate(_) => channel::handle(raw_event, context).await,
+        // Event::ChannelPinsUpdate(_) => Self::handle(raw_event, EventType::ChannelPinsUpdate).await
+        // Event::GuildBanAdd(_) => Self::handle(raw_event, EventType::GuildBanAdd).await,
+        // Event::GuildBanRemove(_) => Self::handle(raw_event, EventType::GuildBanRemove).await,
+        // Event::GuildEmojisUpdate(_) => Self::handle(raw_event, EventType::GuildEmojisUpdate).await
+        // Event::GuildIntegrationsUpdate(_) => Self::handle(raw_event, EventType::GuildIntegrationsUpdate).await
+        // Event::GuildMemberAdd(_) => Self::handle(raw_event, EventType::GuildMemberAdd).await,
+        // Event::GuildMemberRemove(_) => Self::handle(raw_event, EventType::GuildMemberRemove).await
+        // Event::GuildMemberUpdate(_) => Self::handle(raw_event, EventType::GuildMemberUpdate).await
+        // Event::GuildMembersChunk(_) => Self::handle(raw_event, EventType::GuildMembersChunk).await
+        // Event::GuildRoleCreate(_) => Self::handle(raw_event, EventType::GuildRoleCreate).await,
+        // Event::GuildRoleDelete(_) => Self::handle(raw_event, EventType::GuildRoleDelete).await,
+        // Event::GuildRoleUpdate(_) => Self::handle(raw_event, EventType::GuildRoleUpdate).await,
+        // Event::GuildUnavailable(_) => Self::handle(raw_event, EventType::GuildUnavailable).await
+        // Event::GuildUpdate(_) => Self::handle(raw_event, EventType::GuildUpdate).await,
+        // Event::MessageCreate(_) => Self::handle(raw_event, EventType::MessageCreate).await,
+        // Event::MessageDelete(_) => Self::handle(raw_event, EventType::MessageDelete).await,
+        // Event::MessageDeleteBulk(_) => Self::handle(raw_event, EventType::MessageDeleteBulk).await
+        // Event::MessageUpdate(_) => Self::handle(raw_event, EventType::MessageUpdate).await,
+        // Event::ReactionAdd(_) => Self::handle(raw_event, EventType::ReactionAdd).await,
+        // Event::ReactionRemove(_) => Self::handle(raw_event, EventType::ReactionRemove).await,
+        // Event::ReactionRemoveAll(_) => Self::handle(raw_event, EventType::ReactionRemoveAll).await
+        // Event::VoiceStateUpdate(_) => Self::handle(raw_event, EventType::VoiceStateUpdate).await
+        // Event::VoiceServerUpdate(_) => Self::handle(raw_event, EventType::VoiceServerUpdate).await
+        // Event::WebhookUpdate(_) => Self::handle(raw_event, EventType::WebhookUpdate).await,
+        Event::Unknown(event) => {
+            warn!("Received Event::Unknown: {:?}", event);
+            Some(NormalizedEvent {
+                action_type: ActionType::Unknown,
+                id: context.id_provisioner.provision(),
+                source: Source::gateway(&event),
+                origin: ActionOrigin::Gateway,
+                timestamp: time::millisecond_ts(),
+                subject_id: None,
+                guild_id: None,
+                agent_id: None,
+                audit_log_id: None,
+            })
+        },
+        _ => None
     }
 }
 
