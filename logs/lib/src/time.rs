6de57671d2a64e24d86276f918cfeb739a91d1e0
Joseph Azevedo <joseph.az@gatech.edu>
2020-06-19 00:26:29 -0400

    fix(logs): incorporate feedback; improve syntax

--- logs/lib/src/time.rs
+++ logs/lib/src/time.rs
@@ -7,15 +7,24 @@ pub fn millisecond_ts() -> u64 {
 #[cfg(target_os = "linux")]
 mod time {
     use libc::{clock_gettime, timespec, CLOCK_REALTIME};
-    use std::mem;
 
     /// Invokes `clock_gettime` from time.h in libc to get a `timespec` struct
     fn get_time() -> timespec {
-        let mut tp: timespec = unsafe { mem::zeroed() };
-        unsafe {
-            clock_gettime(CLOCK_REALTIME, &mut tp);
+        let mut tp_out = timespec {
+            tv_nsec: 0i64,
+            tv_sec: 0i64
+        };
+        
+        // unsafe needed for FFI call to libc
+        // (it's (almost?) impossible for this call to break safety)
+        let result = unsafe { clock_gettime(CLOCK_REALTIME, &mut tp_out) };
+        if result != 0 {
+            // this should be impossible and indicates something is very wrong
+            // see https://linux.die.net/man/3/clock_gettime
+            panic!("clock_gettime returned non-zero result code")
         }
-        tp
+
+        tp_out
     }
 
     pub fn millisecond_ts() -> u64 {
