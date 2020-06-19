fbfcc371b44fa4379d318e29dc82355f744a90ad
James Johnson <jjohnson474@gatech.edu>
2020-06-19 21:28:47 +0000

    fix make deleting more efficient and better behavior in edge cases

--- shard/src/ext/purge_command.py
+++ shard/src/ext/purge_command.py
@@ -1,10 +1,14 @@
 import discord
 from discord.ext import commands
+from discord.utils import snowflake_time
 
 import re
 from datetime import datetime, timedelta
 
 TIME_REGEX = re.compile(r"(\d+)([m|s])")
+
+# How far into the message history to search for messages to delete
+MESSAGE_LIMIT = 10000
 
 
 @commands.group()
@@ -30,10 +34,11 @@ async def purge(ctx):
     from the target message's author in the channel will be deleted depending on the
     timing of when the original message was deleted.
     '''
-    # Both subcommands will attempt to use the more efficient channel.delete_messages method
-    # when possible. This operation is only valid when attempting to delete 100
-    # or fewer messages. When this is not possible, the channel.purge method will
-    # be used instead.
+    settings = ctx.bot.settings[ctx.guild]
+    if ctx.author.id not in settings.admins_ids:
+        await ctx.send("You do not have permissions to purge messaages")
+        return
+
     if ctx.invoked_subcommand is None:
         await ctx.send("Need to use command with id or time parameter")
         return
@@ -57,9 +62,11 @@ async def id(ctx, mid, inclusive=False):
     when this command is run, then all of that user's messages may not get
     deleted.
 
-    NOTE: If the original message is deleted before the bot deletes it, all messages
-    from the target message's author in the channel will be deleted depending on the
-    timing of when the original message was deleted.
+    NOTE: If the original message is deleted before the bot deletes it and after
+    Architus has already started to go through the message history, all messages
+    from the target message's author (up to 10,000) in the channel will be deleted.
+    This race condition should have no effect on the command's behavior if
+    all messages are going to be deleted.
     """
     message_id = None
     try:
@@ -68,17 +75,14 @@ async def id(ctx, mid, inclusive=False):
         await ctx.send("Message ID has an invalid format")
         return
 
-    settings = ctx.bot.settings[ctx.guild]
-    if ctx.author.id not in settings.admins_ids:
-        await ctx.send("You do not have permissions to purge messaages")
-        return
-
     try:
         original_message = await ctx.channel.fetch_message(message_id)
     except discord.NotFound:
-        await ctx.send("Original message has already been deleted. "
-                       "Try again with different message.")
-        return
+        if not inclusive:
+            await ctx.send("Message was already deleted and inclusive mod is not set")
+            return
+        sent_time = snowflake_time(message_id)
+        original_message = None
     except discord.Forbidden:
         await ctx.send("Architus does not have permission to read message history")
         return
@@ -90,27 +94,24 @@ async def id(ctx, mid, inclusive=False):
         return
 
     messages_to_delete = []
-    async for m in ctx.channel.history(limit=500):
+    args = dict()
+    if original_message is None:
+        args['after'] = sent_time
+    async for m in ctx.channel.history(limit=MESSAGE_LIMIT, **args):
         if inclusive or m.author == original_message.author:
             messages_to_delete.append(m)
-        if m.id == original_message.id:
+        if original_message is not None and m.id == original_message.id:
             break
 
     try:
-        if len(messages_to_delete) <= 100:
-            async with ctx.channel.typing():
-                await ctx.channel.delete_messages(messages_to_delete)
-                await ctx.send(f"Deleted {len(messages_to_delete)} message(s)")
-        else:
-            ids = list(map(lambda m: m.id, messages_to_delete))
-            async with ctx.channel.typing():
-                deleted = await ctx.channel.purge(limit=len(messages_to_delete), check=lambda m: m.id in ids,
-                                                  bulk=False)
-                await ctx.send(f"Deleted {len(deleted)} message(s)")
+        async with ctx.channel.typing():
+            for i in range(0, len(messages_to_delete), 100):
+                await ctx.channel.delete_messages(messages_to_delete[i:i + 100])
+            await ctx.send(f"Deleted {len(messages_to_delete)} message(s)")
     except discord.Forbidden:
         await ctx.send("Architus does not have permission to remove messages in this server")
     except discord.HTTPException:
-        await ctx.send("Failed to reach discord serverrs. Try again in a bit")
+        await ctx.send("Failed to reach discord servers. Try again in a bit")
     except Exception:
         await ctx.send("Something went wrong. :(")
 
@@ -149,13 +150,8 @@ async def time(ctx, time_window):
 
     earliest = now - diff
 
-    settings = ctx.bot.settings[ctx.guild]
-    if ctx.author.id not in settings.admins_ids:
-        await ctx.send("You do not have permissions to purge messaages")
-        return
-
     try:
-        messages_to_delete = await ctx.channel.history(limit=500, after=earliest).flatten()
+        messages_to_delete = await ctx.channel.history(limit=MESSAGE_LIMIT, after=earliest).flatten()
     except discord.Forbidden:
         await ctx.send("Architus does not have permission to read message history")
         return
@@ -167,16 +163,10 @@ async def time(ctx, time_window):
         return
 
     try:
-        if len(messages_to_delete) <= 100:
-            async with ctx.channel.typing():
-                await ctx.channel.delete_messages(messages_to_delete)
-                await ctx.send(f"Deleted {len(messages_to_delete)} message(s)")
-        else:
-            ids = list(map(lambda m: m.id, messages_to_delete))
-            async with ctx.channel.typing():
-                deleted = await ctx.channel.purge(limit=len(messages_to_delete), check=lambda m: m.id in ids,
-                                                  bulk=False)
-                await ctx.send(f"Deleted {len(deleted)} message(s)")
+        async with ctx.channel.typing():
+            for i in rane(0, len(messages_to_delete), 100):
+                await ctx.channel.delete_messages(messages_to_delete[i:i + 100])
+            await ctx.send(f"Deleted {len(messages_to_delete)} message(s)")
     except discord.Forbidden:
         await ctx.send("Architus does not have permission to remove messages "
                        "in this server.")
