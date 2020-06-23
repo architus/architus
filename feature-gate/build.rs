37356304766086dd47ad5ba6c36a6e3bd338194c
James Johnson <jjohnson474@gatech.edu>
2020-06-23 23:47:57 +0000

    fix broken build

--- feature-gate/build.rs
+++ feature-gate/build.rs
@@ -1,11 +1,6 @@
 use std::process::Command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    Command::new("cp")
-        .arg("../lib/ipc/proto/feature-gate.proto")
-        .arg("./proto")
-        .status()?;
-
     tonic_build::configure()
         .build_client(false)
         .build_server(true)
