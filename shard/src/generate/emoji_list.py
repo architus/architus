0be049f04790f19d67498bc1fab29c03cc4a7e78
James Johnson <jjohnson474@gatech.edu>
2020-06-23 23:54:43 +0000

    feat start embed scroll thing

--- shard/src/generate/emoji_list.py
+++ shard/src/generate/emoji_list.py
@@ -4,25 +4,41 @@ from io import BytesIO
 font = ImageFont.truetype("res/fonts/uni-sans-semi-bold-webfont.ttf", 32)
 font_color = (218, 219, 220, 255)
 color = (47, 49, 54, 255)
+emojis_per_page = 12
 
 SIZE = 64
 
 def generate(emojis):
     """
-    Transform a list of emojis into an image
+    Transform a list of emojis into a list of images.
     """
 
-    im = Image.new('RGBA', (600, int(len(emojis) * SIZE * 1.3) + SIZE // 4), color)
+    """
+    X_SIZE = max(1, len(emojis) // 15) * 480
+    Y_SIZE = int(max(15, len(emojis)) * SIZE * 1.3 + 16)
+    im = Image.new("RGBA", (X_SIZE, Y_SIZE), color)
+    """
+    """
+    if len(emojis) > 25:
+        im = Image.new('RGBA', (1200, 2080 + SIZE // 4), color)
+    else:
+    """
+    images = []
+    for v in range(0, len(emojis), emojis_per_page):
+        im = Image.new('RGBA', (480, int(len(emojis[v:v + emojis_per_page]) * SIZE * 1.3) + SIZE // 4), color)
 
-    for (i, e) in enumerate(emojis):
-        im.alpha_composite(e.im.resize((SIZE, SIZE)), (16, int(i * SIZE * 1.3) + 16))
-        d = ImageDraw.Draw(im)
+        for (i, e) in enumerate(emojis[v:v + emojis_per_page]):
+            x = 16
+            y = int(i * SIZE * 1.3) + 16
+            im.alpha_composite(e.im.resize((SIZE, SIZE)), (x, y))
+            d = ImageDraw.Draw(im)
 
-        d.text((96, int(i * SIZE * 1.3) + 16 + 8), f":{e.name}:", fill=font_color, font=font)
+            d.text((x + 80, y + 8), f":{e.name}:", fill=font_color, font=font)
 
-    buf = BytesIO()
-    im.save(buf, format="PNG")
-    buf.seek(0)
-    return buf
+        buf = BytesIO()
+        im.save(buf, format="PNG")
+        buf.seek(0)
+        images.append(buf)
+    return images
 
     #im.show()
