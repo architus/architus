9c051de4fb619d0288eb060c8c5ae1e27db7277b
James Johnson <jjohnson474@gatech.edu>
2020-06-23 05:11:36 +0000

    fix remove weird boxees around emojis

--- shard/src/generate/emoji_list.py
+++ shard/src/generate/emoji_list.py
@@ -2,18 +2,20 @@ from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 
 font = ImageFont.truetype("res/fonts/uni-sans-semi-bold-webfont.ttf", 32)
-font_color = (218, 219, 220)
-color = (47,49,54)
+font_color = (218, 219, 220, 255)
+color = (47, 49, 54, 255)
 
 SIZE = 64
 
 def generate(emojis):
-    '''takes a list of architus emojis and makes an image'''
+    """
+    Transform a list of emojis into an image
+    """
 
-    im = Image.new('RGB', (600, int(len(emojis) * SIZE * 1.3) + SIZE // 4), color)
+    im = Image.new('RGBA', (600, int(len(emojis) * SIZE * 1.3) + SIZE // 4), color)
 
     for (i, e) in enumerate(emojis):
-        im.paste(e.im.resize((SIZE, SIZE)), (16, int(i * SIZE * 1.3) + 16))
+        im.alpha_composite(e.im.resize((SIZE, SIZE)), (16, int(i * SIZE * 1.3) + 16))
         d = ImageDraw.Draw(im)
 
         d.text((96, int(i * SIZE * 1.3) + 16 + 8), f":{e.name}:", fill=font_color, font=font)
