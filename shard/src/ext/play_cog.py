67d0cf3d3ed5fe01b391cdd9809e5d7cd0084eb0
Jonathan Buchanan <johnyburd8@gmail.com>
2020-11-06 16:23:24 -0500

    how did I miss this

--- shard/src/ext/play_cog.py
+++ shard/src/ext/play_cog.py
@@ -5,29 +5,30 @@ from src.voice_manager import VoiceManager, Song
 from src.utils import doc_url
 from lib.config import logger
 import discord
-import asyncio
+import functools
 
 
-def (cmd):
-    """Adds the restriction that a command must be run in a bot commands channel if the guild has one set
-    Does nothing if not in a guild
-    Requires that the command be in the context of a cog that has the attribute `self.bot`
-    """
+def requires_voice(cmd):
     @functools.wraps(cmd)
-    async def bc_cmd(self, ctx, *args, **kwargs):
+    async def new_cmd(self, ctx, *args, **kwargs):
         if ctx.guild:
             settings = self.bot.settings[ctx.guild]
-            if settings.bot_commands_channels\
-                    and ctx.channel.id not in settings.bot_commands_channels\
-                    and ctx.author.id not in settings.admin_ids:
-
-                for channel_id in settings.bot_commands_channels:
-                    bc_ch = discord.utils.get(ctx.guild.channels, id=channel_id)
-                    if bc_ch:
-                        await ctx.send(f"Please use {bc_ch.mention} for that command")
+            manager = self.voice_managers[ctx.guild.id]
+            if not settings.music_enabled:
+                logger.debug(f"music commands disabled for {ctx.guild.id}")
+                return
+            if ch := manager.channel:
+                if ctx.author.voice and (author_ch := ctx.author.voice.channel):
+                    if ch.id != author_ch.id and ctx.author.id not in settings.admin_ids:
+                        await ctx.send(f"Please join {ch} to use music commands :)")
                         return
+            if not (ctx.author.voice and ctx.author.voice.channel):
+                await ctx.send("Please join a voice channel to use music commands :)")
+                return
+            await manager.join(ctx.author.voice.channel)
+            ctx.voice_manager = manager
         return await cmd(self, ctx, *args, **kwargs)
-    return bc_cmd
+    return new_cmd
 
 
 class VoiceManagers(dict):
@@ -41,7 +42,7 @@ class VoiceManagers(dict):
             if not guild:
                 raise KeyError(f"No guild found for id: {key}")
             self[key] = VoiceManager(self.bot, self.bot.get_guild(key))
-        return self[key]uuu
+        return self[key]
 
 
 class VoiceCog(commands.Cog, name="Voice"):
@@ -54,11 +55,6 @@ class VoiceCog(commands.Cog, name="Voice"):
         m = self.voice_managers[ctx.guild.id]
         while m.linebuffer:
             await ctx.send(m.linebuffer.pop(0))
-
-
-    @commands.Cog.listener()
-    async def on_guild_join(self, guild):
-        self.voice_managers[guild.id] = VoiceManager(self.bot, guild)
 
     async def enqueue(self, ctx, query: str) -> List[Song]:
         manager = self.voice_managers[ctx.guild.id]
@@ -82,78 +78,83 @@ class VoiceCog(commands.Cog, name="Voice"):
 
     @commands.command(aliases=['p'])
     @doc_url("https://docs.archit.us/commands/music/#play")
+    @requires_voice
     async def play(self, ctx, *song: str):
         '''play <search item|youtube url|spotify url>
         Add a song to the music queue.
         Supports youtube and spotify links.
         '''
-        manager = self.voice_managers[ctx.guild.id]
-        settings = self.bot.settings[ctx.guild]
-        if ctx.author.voice and (ch := ctx.author.voice.channel):
-            if manager.channel and ch.id != manager.channel.id:
-                if ctx.author.id not in settings.admin_ids:
-                    ctx.send("Please join a voice channel to use music commands")
-                    return
-            await manager.join(ch)
-        else:
-            ctx.send("Please join a voice channel to use music commands")
-            return
-
         arg = " ".join(song)
         async with ctx.channel.typing():
             if len(await self.enqueue(ctx, arg)) < 1:
                 return
 
-            if manager.is_playing:
+            if ctx.voice_manager.is_playing:
                 pass
             else:
                 try:
-                    song = await manager.play()
+                    song = await ctx.voice_manager.play()
                 except Exception as e:
                     logger.exception("")
                     await ctx.send(f"error playing music {e}")
                 else:
                     msg = song.name if 'youtu' in arg else song.url
                     await ctx.send(f"now playing: {msg}")
-    @commands.command()
-    async def skip(self, ctx):
-        '''
-        requires author in vc
-        requires bot in vc & playing
-        '''
-        manager = self.VoiceManagers[ctx.guild.id]
 
+    @commands.command()
+    @doc_url("https://docs.archit.us/commands/music/#skip")
+    @requires_voice
+    async def skip(self, ctx):
+        try:
+            song = await ctx.voice_manager.play()
+        except IndexError:
+            await ctx.send("no more songs, goodbye")
+            await ctx.voice_manager.disconnect()
+        else:
+            await ctx.send(f"now playing: {song.name}")
 
     @commands.group(aliases=["q"])
-    @doc_url("https://docs.archit.us/commands/")
+    @doc_url("https://docs.archit.us/commands/#queue")
     async def queue(self, ctx):
         '''queue [add|rm|show|{song}] args'''
         if ctx.invoked_subcommand is None:
             await ctx.send(embed=self.voice_managers[ctx.guild.id].q.embed())
 
     @queue.command(aliases=['a'])
+    @doc_url("https://docs.archit.us/commands/#queue")
+    @requires_voice
     async def add(self, ctx, *query):
         '''queue add <songs>'''
         await self.enqueue(ctx, " ".join(query))
 
     @queue.command(aliases=['remove', 'r'])
+    @doc_url("https://docs.archit.us/commands/#queue")
+    @requires_voice
     async def rm(self, ctx, index: int):
         '''queue rm <index>'''
-        manager = self.voice_managers[ctx.guild.id]
+        q = ctx.voice_manager
         try:
-            song = manager.q[index - 1]
-            del manager.q[index - 1]
+            song = q[index - 1]
+            del q[index - 1]
         except IndexError:
             await ctx.send("not sure what song you're talking about")
         else:
             await ctx.send(f"removed *{song.name}*")
 
     @queue.command()
+    @doc_url("https://docs.archit.us/commands/music/#clear")
+    @requires_voice
+    async def clear(self, ctx):
+        n = ctx.voice_manager.q.clear()
+        await ctx.send(f"cleared {n} songs from queue")
+
+    @queue.command()
+    @doc_url("https://docs.archit.us/commands/music/#clear")
     async def shuffle(self, ctx):
         '''queue shuffle'''
-        q = self.voice_managers[ctx.guild.id].q
-        q.shuffle = not q.shuffle
-        await ctx.send(f"Shuffle is **{'on' if q.shuffle else 'off'}**")
+        manager = self.voice_managers[ctx.guild.id]
+        manager.q.shuffle = not manager.q.shuffle
+        await ctx.send(f"Shuffle is **{'on' if manager.q.shuffle else 'off'}**")
 
 
 class BPlay(commands.Cog, name="Music Player"):
@@ -278,7 +279,6 @@ class BPlay(commands.Cog, name="Music Player"):
             await ctx.channel.send("No songs left. goodbye")
 
     @commands.command()
-    @doc_url("https://docs.archit.us/commands/music/#clear")
     async def clear(self, ctx):
         '''clear
         Clear all songs from queue.'''
