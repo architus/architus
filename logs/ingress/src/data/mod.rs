e4e40557bdef3f3d88147d56920e8de0dc2b5452
Joseph Azevedo <joseph.az@gatech.edu>
2020-11-24 19:25:34 -0500

    feat: re-write ingress entrypoint to use twilight

--- logs/ingress/src/data/mod.rs
+++ logs/ingress/src/data/mod.rs
@@ -1,20 +1,24 @@
 mod processor;
 
-use std::collections::HashMap;
-use logs_lib::{ActionType, AuditLogEntryType};
 use crate::data::processor::{GatewayProcessor, Path, TimestampSource};
+use logs_lib::{ActionType, AuditLogEntryType};
+use std::collections::HashMap;
+
+
 
 fn make_definitions() -> HashMap<AuditLogEntryType, GatewayProcessor> {
     let mut map: HashMap<AuditLogEntryType, GatewayProcessor> = HashMap::new();
 
-    map.insert(AuditLogEntryType::ChannelCreate, GatewayProcessor{
-        action_type: ActionType::ChannelCreate,
-        timestamp_src: TimestampSource::Snowflake(Path::gateway("id")),
-        subject_id_src: Some(Path::gateway("id")),
-        agent_id_src: Some(Path::audit_log("user_id")),
-        audit_log_src: None,
-    });
+    map.insert(
+        AuditLogEntryType::ChannelCreate,
+        GatewayProcessor {
+            action_type: ActionType::ChannelCreate,
+            timestamp_src: TimestampSource::Snowflake(Path::gateway("id")),
+            subject_id_src: Some(Path::gateway("id")),
+            agent_id_src: Some(Path::audit_log("user_id")),
+            audit_log_src: None,
+        },
+    );
 
     map
 }
-
--- logs/ingress/src/main.rs
+++ logs/ingress/src/main.rs
@@ -1,42 +1,81 @@
-mod audit_log;
-mod data;
-mod env;
+// mod audit_log;
+mod config;
+// mod data;
 mod event;
-mod gateway;
+// mod gateway;
 
-use env::Environment;
+use anyhow::{Context, Result};
+use clap::{App, Arg};
+use config::Configuration;
+use lazy_static::lazy_static;
 use log::{error, info};
-use serenity::async_trait;
-use serenity::framework::standard::StandardFramework;
-use serenity::model::gateway::Ready;
-use serenity::prelude::*;
+use futures::StreamExt;
+use tokio_compat_02::FutureExt;
+use twilight_gateway::{Event, EventTypeFlags, Intents, Shard};
+use twilight_model::gateway::event::shard::Payload;
 
 /// Bootstraps the bot and begins listening for gateway events
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let env = Environment::load();
-
-    let framework = StandardFramework::new();
-    let mut client = Client::new(&env.token)
-        .event_handler(StatusHandler)
-        .raw_event_handler(gateway::Handler::new())
-        .intents(*gateway::INTENTS)
-        .framework(framework)
-        .await
-        .expect("error creating client");
-
-    if let Err(why) = client.start().await {
-        error!("Client error: {:?}", why);
+    match run().compat().await {
+        Ok(_) => info!("Exiting"),
+        Err(err) => error!("{:?}", err),
     }
 }
 
-struct StatusHandler;
+lazy_static! {
+    /// Includes all guild-related events to signal to Discord that we intend to
+    /// receive and process them
+    pub static ref INTENTS: Intents = Intents::GUILDS
+        | Intents::GUILD_MEMBERS
+        | Intents::GUILD_BANS
+        | Intents::GUILD_EMOJIS
+        | Intents::GUILD_INTEGRATIONS
+        | Intents::GUILD_WEBHOOKS
+        | Intents::GUILD_INVITES
+        | Intents::GUILD_VOICE_STATES
+        | Intents::GUILD_MEMBERS
+        | Intents::GUILD_MESSAGES
+        | Intents::GUILD_MESSAGE_REACTIONS;
+}
 
-#[async_trait]
-impl EventHandler for StatusHandler {
-    /// Called once bot connects and is ready
-    async fn ready(&self, _ctx: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.tag());
+/// Attempts to initialize the bot and listen for gateway events
+async fn run() -> Result<()> {
+    // Use clap to pass in a config path
+    let app = App::new("logs-ingress").arg(
+        Arg::with_name("config")
+            .short("c")
+            .long("config")
+            .value_name("FILE")
+            .help("TOML config file path")
+            .takes_value(true)
+            .required(true),
+    );
+    let matches = app.get_matches();
+    let config_path = matches.value_of("config").unwrap();
+
+    // Parse the config from the path and use it to initialize the event stream
+    let config = Configuration::try_load(config_path)?;
+    let event_types = EventTypeFlags::SHARD_PAYLOAD;
+    let mut shard = Shard::new(config.secrets.discord_token, *INTENTS);
+    let mut events = shard.some_events(event_types);
+
+    shard.start().await.context("Could not start shard")?;
+    info!("Created shard and preparing to listen for gateway events");
+
+    // Listen for all raw gateway events and process them
+    while let Some(event) = events.next().await {
+        match event {
+            Event::ShardPayload(Payload{ bytes }) => {
+                if let Ok(as_str) = std::str::from_utf8(&bytes) {
+                    // TODO consume
+                    println!("Event: {}", as_str);
+                }
+            },
+            _ => {},
+        }
     }
+
+    Ok(())
 }
