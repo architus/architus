f37f2f35558038ac77c5e6fa3feb34e87ace748a
Joseph Azevedo <joseph.az@gatech.edu>
2020-12-03 07:08:43 -0500

    feat(logs): add basic filtering via GraphQL

--- logs/logging/build.rs
+++ logs/logging/build.rs
@@ -15,6 +15,19 @@ fn main() -> Result<()> {
         )
         .type_attribute(".Logging.EventOrigin", "#[derive(::juniper::GraphQLEnum)]")
         .type_attribute(".Logging.EventType", "#[derive(::juniper::GraphQLEnum)]")
+        .type_attribute(".Logging.EventOrigin", "#[derive(::strum::EnumString)]")
+        .type_attribute(".Logging.EventType", "#[derive(::strum::EnumString)]")
+        // Note: we have to hope (and pray) that the strum serializations are the same as the juniper ones
+        // The book (https://graphql-rust.github.io/juniper/current/types/enums.html#enums) is wrong
+        // when it says they are uppercase; they should be SCREAMING_SNAKE_CASE in juniper as well
+        .type_attribute(
+            ".Logging.EventOrigin",
+            r#"#[strum(serialize_all = "SCREAMING_SNAKE_CASE")]"#,
+        )
+        .type_attribute(
+            ".Logging.EventType",
+            r#"#[strum(serialize_all = "SCREAMING_SNAKE_CASE")]"#,
+        )
         .compile(&["logging.proto"], &["../../lib/ipc/proto"])
         .context("Compiling logging.proto definitions")?;
 
