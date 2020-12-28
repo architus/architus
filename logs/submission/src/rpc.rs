33ee7f8115afd9b1689e60d3229684585ffc4373
Joseph Azevedo <joseph.az@gatech.edu>
2020-12-28 15:11:22 -0500

    feat(logs): update submission to use revision protobuf

--- logs/submission/src/rpc.rs
+++ logs/submission/src/rpc.rs
@@ -9,3 +9,15 @@ pub mod submission {
 
     pub use generated::*;
 }
+
+pub mod revision {
+    mod generated {
+        // Ignore clippy linting on generated code
+        #![allow(clippy::all, clippy::pedantic, clippy::nursery)]
+        tonic::include_proto!("logs.revision");
+    }
+
+    pub use generated::*;
+    pub type Client =
+        generated::revision_service_client::RevisionServiceClient<tonic::transport::Channel>;
+}
