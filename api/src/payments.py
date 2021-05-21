8cba04eaad9d1c47d0aec07b69b4440eb45f6625
Jonathan Buchanan <johnyburd8@gmail.com>
2021-05-21 13:04:11 -0400

    idk

--- api/src/payments.py
+++ api/src/payments.py
@@ -1,9 +1,10 @@
 import json
 
 import stripe
+from flask import request
 
 from src.util import CustomResource
-from lib.config import stripe_secret_key
+from lib.config import logger, stripe_secret_key, stripe_webhook_secret
 
 
 stripe.api_key = stripe_secret_key
@@ -29,3 +30,36 @@ class Checkout(CustomResource):
             cancel_url='https://example.com/cancel',
         )
         return json.dumps({'id': session.id})
+
+
+class Stripe(CustomResource):
+    def post(self):
+        # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
+        signature = request.headers.get('stripe-signature')
+        try:
+            event = stripe.Webhook.construct_event(
+                payload=request.data, sig_header=signature, secret=stripe_webhook_secret)
+            data = event['data']
+        except Exception as e:
+            return e
+        # Get the type of webhook event sent - used to check the status of PaymentIntents.
+        event_type = event['type']
+
+        if event_type == 'checkout.session.completed':
+            # Payment is successful and the subscription is created.
+            # You should provision the subscription and save the customer ID to your database.
+            print(data)
+        elif event_type == 'invoice.paid':
+            # Continue to provision the subscription as payments continue to be made.
+            # Store the status in your database and check when a user accesses your service.
+            # This approach helps you avoid hitting rate limits.
+            print(data)
+        elif event_type == 'invoice.payment_failed':
+            # The payment failed or the customer does not have a valid payment method.
+            # The subscription becomes past_due. Notify your customer and send them to the
+            # customer portal to update their payment information.
+            print(data)
+        else:
+            print('Unhandled event type {}'.format(event_type))
+
+        return {'status': 'success'}
