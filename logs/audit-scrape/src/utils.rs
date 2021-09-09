950e2fc2a226cd513615c7997a2485cc95054efd
James Johnson <jjohnsonjj1251@gmail.com>
2021-09-09 03:38:07 +0000

    Add helper for extracting snowflake to system time

--- logs/audit-scrape/src/utils.rs
+++ logs/audit-scrape/src/utils.rs
@@ -1,3 +1,4 @@
+use std::time::{Duration, SystemTime};
 /// Utility module for dealing with discord snowflakes
 pub mod snowflake {
     pub const DISCORD_EPOCH_OFFSET: u64 = 1_420_070_400_000;
@@ -6,5 +7,18 @@ pub mod snowflake {
 
     pub const fn extract_timestamp(snowflake: u64) -> u64 {
         (snowflake >> TIMESTAMP_BIT_OFFSET) + DISCORD_EPOCH_OFFSET
+    }
+
+    /// Gets a system time from the snowflake. This function may panic
+    /// if the resulting time cannot be represented by a SystemTime.
+    /// That would imply that some snowflake occurred well over
+    /// 200_000_000 years into the future so I'm not going to worry
+    /// about that and just return a SystemTime and not an
+    /// Option<SystemTime>.
+    pub fn extract_system_time(snowflake: u64) -> SystemTime {
+        let millis = extract_timestamp(snowflake);
+        let dur = Duration::from_millis(millis);
+
+        SystemTime::UNIX_EPOCH + dur
     }
 }
