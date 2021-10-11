bf431ae351c2947a9dff24aba35b7b116220d630
Joseph Azevedo <joseph.az@gatech.edu>
2021-10-10 23:13:03 -0400

    ensure paths are valid to lib/python-common & lib/proto

--- sandbox/build_grpc.sh
+++ sandbox/build_grpc.sh
@@ -2,7 +2,7 @@
 
 protoc --go_out=./rpc --go_opt=paths=source_relative \
     --go-grpc_out=./rpc --go-grpc_opt=paths=source_relative \
-    --proto_path=../lib/ipc/proto ../lib/ipc/proto/sandbox.proto
+    --proto_path=../lib/proto ../lib/proto/sandbox.proto
 
-python3 -m grpc_tools.protoc -I../lib/ipc/proto --python_out=./test \
-    --grpc_python_out=./test ../lib/ipc/proto/sandbox.proto
+python3 -m grpc_tools.protoc -I../lib/proto --python_out=./test \
+    --grpc_python_out=./test ../lib/proto/sandbox.proto
