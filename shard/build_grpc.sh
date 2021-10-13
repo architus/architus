a40e1287651fd27c54d64d613e9f2e82ac99079e
Joseph Azevedo <joseph.az@gatech.edu>
2021-10-13 03:35:01 -0400

    clean up whitespace in diff

--- shard/build_grpc.sh
+++ shard/build_grpc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-python3 -m grpc_tools.protoc -I../lib/proto --python_out=../lib/python-common/ipc  --grpc_python_out=../lib/python-common/ipc  \
+python3 -m grpc_tools.protoc -I../lib/proto --python_out=../lib/python-common/ipc --grpc_python_out=../lib/python-common/ipc \
     ../lib/proto/sandbox.proto
 
-python3 -m grpc_tools.protoc -I../lib/proto --python_out=../lib/python-common/ipc  --grpc_python_out=../lib/python-common/ipc \
+python3 -m grpc_tools.protoc -I../lib/proto --python_out=../lib/python-common/ipc --grpc_python_out=../lib/python-common/ipc \
     ../lib/proto/feature-gate.proto
 
-python3 -m grpc_tools.protoc -I../lib/proto --python_out=../lib/python-common/ipc  --grpc_python_out=../lib/python-common/ipc \
+python3 -m grpc_tools.protoc -I../lib/proto --python_out=../lib/python-common/ipc --grpc_python_out=../lib/python-common/ipc \
     ../lib/proto/manager.proto
